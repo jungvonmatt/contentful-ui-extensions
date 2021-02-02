8d1c0a41c21003c7b9eade035cfad2c82bdfe77d
Ben Zörb <ben@sommerlaune.com>
2021-02-02 12:53:24 +0100

    Bump dependencies & switch to babel.config.json

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ const config = {
         use: {
           loader: 'babel-loader',
           options: {
-            configFile: path.resolve(__dirname, 'babel.config.js'),
+            configFile: path.resolve(__dirname, 'babel.config.json'),
           },
         },
       },
